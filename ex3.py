c4ae85d5ed11ed2431e873f7bfa2cb92297c6fce
Eugene Marion Jennings <friendlysquidhacker@gmail.com>
2016-05-25 00:31:00 -0400

    Finishing Exercise 3 of Learn Python the Hard Way, 3rd edition.

--- ex3.py
+++ ex3.py
@@ -1,23 +1,41 @@
+# prints out a string
 print "I will now count my chickens:"
 
+# prints a string, divides 30 by 6 and adds 20.
 print "Hens", 25 + 30 / 6
+
+# prints a string. Then multiplies 25 by 3, calculate the remainder of 75 / 4, and subtracts that remainder from 100. 
 print "Roosters", 100 - 25 * 3 % 4
 
+# prints another string.
 print "Now I will count the eggs:"
 
+# first, python calculates the remainder of 4 / 2, which is 0, and divides 1 / 4, which would be 0.25 in float but rounds down to 0 in integer math. Then it's all addition and subtraction. 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# prints a string.
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Evaluates whether 3 + 2 is less than 5 - 7 and returns a boolean, which prints.
 print 3 + 2 < 5 - 7
 
+# prints a string, then adds two digits and returns an int, which also prints.
 print "What is 3 + 2?", 3 + 2
+
+# prints a string, then subtracts two digits and returns an int, which also prints.
 print "What is 5 - 7?", 5 - 7
 
+# prints a string.
 print "Oh, that's why it's False."
 
+# prints (yet another) string.
 print "How about some more."
 
+# plays with barbies. Also known as printing another string. Then evaluates whether one number is greater than another and prints the boolean.
 print "Is it greater?", 5 > -2
+
+# prints a string, evaluates whether one number is either greater than or equal to the other, prints the boolean.
 print "Is it greater or equal?", 5 >= -2
+
+# prints a string, evaluates whether the first is less than or equal to the second one, prints the boolean.
 print "Is it less or equal?", 5 <= -2
