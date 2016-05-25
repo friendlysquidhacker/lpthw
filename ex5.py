9598b2330c886647004dd98e7cdf247d229babb1
Eugene Marion Jennings <friendlysquidhacker@gmail.com>
2016-05-25 00:59:10 -0400

    Finished Study Drill 2 for Exercise 5 of Learn Python the Hard Way, 3rd Edition.

--- ex5.py
+++ ex5.py
@@ -1,17 +1,20 @@
 name = "Zed A. Shaw"
 age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
+height_in = 74 # inches
+weight_lb = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+height_cm = height_in * 2.54
+weight_kg = weight_lb / 2.2
+
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
-print "Actually that's not too heavy."
+print "He's %d inches or %d cm tall." % (height_in, height_cm)
+print "He weighs %d pounds or %d kilos." % (weight_lb, weight_kg)
+print "That's not so heavy -- he's probably human."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." %(age, height, weight, age + height + weight)
+print "If I add %d, %d, and %d I get %d." %(age, height_in, weight_lb, age + height_in + weight_lb)
