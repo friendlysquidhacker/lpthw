a46f3a64ebf575c4ac3b545433fc3dc875fcd698
Eugene Marion Jennings <friendlysquidhacker@gmail.com>
2016-05-25 15:59:49 -0400

    Study drills for Exercise 7, Learn Python the Hard Way 3rd edition

--- ex7.py
+++ ex7.py
@@ -1,8 +1,17 @@
+
+# displays a string
 print "Mary had a little lamb."
+
+# displays a string, sets a point to take an argument, includes a string argument
 print "Its fleece was white as %s." % 'snow'
+
+# displays a string
 print "And everywhere that Mary went."
+
+# displays a one-character string ten times
 print "." * 10 # what'd that do?
 
+# this block of code declares 12 individual strings of one character each -- unless these are counted as chars instead?
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,6 +25,11 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+
 # watch that comma at the end. try removing it to see what happens
+
+# This line concatenates and displays six variables as strings and starts a new line
 print end1 + end2 + end3 + end4 + end5 + end6,
+
+# This line concatenates and displays six variables as a string
 print end7 + end8 + end9 + end10 + end11 + end12
