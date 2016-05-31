6e0ddddb59f125d7b241cdb619d57239e46f56c7
Eugene Marion Jennings <friendlysquidhacker@gmail.com>
2016-05-31 10:06:31 -0400

    Starting to tinker a bit.

--- ex13.py
+++ ex13.py
@@ -1,8 +1,14 @@
+# Okay, this seems a lot like a #include statement.
 from sys import argv
 
+# Now I've figured out these are command line arguments.
 script, first, second, third = argv
+# That's THIS filename and three command line args.
+
 
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+#Wooooo! Yeah, modules!
